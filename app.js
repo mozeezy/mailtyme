be4c2ec92f161141ef703e2e4c69ee2f3baa7e85
Mohamed <justmoe23@gmail.com>
2022-02-10 22:39:45 -0500

    set up a server to serve the html file

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 const express = require("express");
 const request = require("request");
 const bodyParser = require("body-parser");
-
 const app = express();
 
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", (req, res) => {
+  const firstName = req.body.firstName;
+  const lastName = req.body.lastName;
+  const email = req.body.email;
+
+  console.log(`My name is ${firstName} ${lastName} and my email is ${email}`);
+});
 
 app.listen(3000, () => {
   console.log("Server is listening on port 3000");
